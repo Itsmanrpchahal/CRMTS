de791f737a3c19ef362d307e3d7e391ca3a3bc6f
CRM_MOBILE <manpreet.webperfection@gmail.com>
2023-10-18 12:24:38 +0530

    ffff

---
+++ android/app/.cxx/Debug/2g444f2e/prefab/x86_64/prefab/lib/x86_64-linux-android/cmake/hermes-engine/hermes-engineConfig.cmake
@@ -0,0 +1,9 @@
+if(NOT TARGET hermes-engine::libhermes)
+add_library(hermes-engine::libhermes SHARED IMPORTED)
+set_target_properties(hermes-engine::libhermes PROPERTIES
+    IMPORTED_LOCATION "/Users/dev/.gradle/caches/transforms-3/928dcabc2498742a9c0904af3441856d/transformed/jetified-hermes-android-0.72.4-debug/prefab/modules/libhermes/libs/android.x86_64/libhermes.so"
+    INTERFACE_INCLUDE_DIRECTORIES "/Users/dev/.gradle/caches/transforms-3/928dcabc2498742a9c0904af3441856d/transformed/jetified-hermes-android-0.72.4-debug/prefab/modules/libhermes/include"
+    INTERFACE_LINK_LIBRARIES ""
+)
+endif()
+
---
+++ android/app/.cxx/RelWithDebInfo/5j4t1q4p/prefab/x86_64/prefab/lib/x86_64-linux-android/cmake/hermes-engine/hermes-engineConfig.cmake
@@ -0,0 +1,9 @@
+if(NOT TARGET hermes-engine::libhermes)
+add_library(hermes-engine::libhermes SHARED IMPORTED)
+set_target_properties(hermes-engine::libhermes PROPERTIES
+    IMPORTED_LOCATION "/Users/dev/.gradle/caches/transforms-3/18d0929b09a991f20656be7a26252a53/transformed/jetified-hermes-android-0.72.4-release/prefab/modules/libhermes/libs/android.x86_64/libhermes.so"
+    INTERFACE_INCLUDE_DIRECTORIES "/Users/dev/.gradle/caches/transforms-3/18d0929b09a991f20656be7a26252a53/transformed/jetified-hermes-android-0.72.4-release/prefab/modules/libhermes/include"
+    INTERFACE_LINK_LIBRARIES ""
+)
+endif()
+
